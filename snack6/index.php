83914ffba05bf46a8de01f9271dbe01fff9c27bb
vincenzovolpe <vinci792010@gmail.com>
2020-01-22 23:41:57 +0100

    Caricamento snack 6

---
+++ snack6/index.php
@@ -0,0 +1,74 @@
+<?php
+
+    $db = [
+        'teachers' => [
+            [
+                'name' => 'Michele',
+                'lastname' => 'Papagni'
+            ],
+            [
+                'name' => 'Fabio',
+                'lastname' => 'Forghieri'
+            ]
+        ],
+        'pm' => [
+            [
+                'name' => 'Roberto',
+                'lastname' => 'Marazzini'
+            ],
+            [
+                'name' => 'Federico',
+                'lastname' => 'Pellegrini'
+            ]
+        ]
+    ];
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Snack 6</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <div class="container">
+            <div class="grigio">
+                <h1>Teachers</h1>
+                <?php
+                foreach ($db as $teachers => $value) {
+                    if ($teachers == "teachers") {
+                        echo "<br>";
+                        foreach ($value as $key) {
+                            echo "<br>";
+                            foreach ($key as $elemento => $valore) {
+                                echo $elemento. ": ".$valore;
+                                echo "<br>";
+                            }
+                        }
+                        echo "<br>";
+                    }
+                }?>
+            </div>
+            <div class="verde">
+                <h1>PM</h1>
+                <?php
+                foreach ($db as $teachers => $value) {
+                    if ($teachers == "pm") {
+                        echo "<br>";
+                        foreach ($value as $key) {
+                            echo "<br>";
+                            foreach ($key as $elemento => $valore) {
+                                echo $elemento. ": ".$valore;
+                                echo "<br>";
+                            }
+                        }
+                        echo "<br>";
+                    }
+                }?>
+            </div>
+        </div>
+    </body>
+</html>
